1fc59ed296b9897769ba1e5e5b93456d0e5fb75f
amWRit <derasrocks@gmail.com>
2017-05-14 18:11:50 +0545

    Added views integrated with adminlte

---
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -0,0 +1,55 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+        <!-- Sidebar user panel (optional) -->
+        @if (! Auth::guest())
+            <div class="user-panel">
+                <div class="pull-left image">
+                    <img src="{{ Gravatar::get($user->email) }}" class="img-circle" alt="User Image" />
+                </div>
+                <div class="pull-left info">
+                    <p>{{ Auth::user()->name }}</p>
+                    <!-- Status -->
+                    <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('adminlte_lang::message.online') }}</a>
+                </div>
+            </div>
+        @endif
+
+        <!-- search form (Optional) -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="{{ trans('adminlte_lang::message.search') }}..."/>
+              <span class="input-group-btn">
+                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+              </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu">
+            <li class="header">MENU</li>
+            <!-- Optionally, you can add icons to the links -->
+            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-home'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
+            <li class="treeview">
+                <a href="#"><i class='fa fa-user-o'></i> <span>Employee</span> <i class="fa fa-angle-down pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="{{url('view_employees')}}">View all</a></li>
+                    <li><a href="{{url('add_employee')}}">Add new</a></li>
+                </ul>
+            </li>
+
+            <li class="treeview">
+                <a href="#"><i class='fa fa-plane'></i> <span>Leave</span> <i class="fa fa-angle-down pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="{{url('view_leaves')}}">View all</a></li>
+                    <li><a href="{{url('add_leave')}}">Add new</a></li>
+                </ul>
+            </li>
+        </ul><!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
